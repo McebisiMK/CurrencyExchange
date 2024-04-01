a0d51a6bf1bbf2cb42be8490552cf6a49e17c545
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2024-04-01 19:51:43 +0200

    Extracted test cases to their own class

---
+++ CurrencyExchange.Tests/TestData/FluentValidationsTestData.cs
@@ -0,0 +1,29 @@
+﻿namespace CurrencyExchange.Tests.TestData
+{
+    [TestFixture]
+    public class FluentValidationsTestData
+    {
+
+        public static readonly object[] AddCurrencyConversionTestCases =
+        {
+            new object[] { "", "currency result", new List<string> { "Base is required!" } },
+            new object[] { "USD", "", new List<string> { "Result is required!" } },
+            new object[] { "", "", new List<string> { "Base is required!", "Result is required!" } },
+        };
+
+        public static readonly object[] AddCurrencyRatesTestCases =
+        {
+            new object[] { "", "rate results", new List<string> { "Base is required!" } },
+            new object[] { "USD", "", new List<string> { "Results is required!" } },
+            new object[] { "", "", new List<string> { "Base is required!", "Results is required!" } },
+        };
+        
+        public static readonly object[] ConvertCurrencyTestCases =
+        {
+            new object[] { "", "ZAR", 100M, new List<string> { "Base is required!" } },
+            new object[] { "USD", "", 100M, new List<string> { "Target is required!" } },
+            new object[] { "USD", "ZAR", 0M, new List<string> { "Amount is required!"} },
+            new object[] { "", "", 0M, new List<string> { "Base is required!", "Target is required!", "Amount is required!" } },
+        };
+    }
+}
--- CurrencyExchange.Tests/UnitTests/FluentValidatorsTests.cs
+++ CurrencyExchange.Tests/UnitTests/FluentValidatorsTests.cs
@@ -2,35 +2,14 @@
 using CurrencyExchange.Application.Commands.CurrencyRates.Add;
 using CurrencyExchange.Application.Queries.CurrencyConversions.Convert;
 using CurrencyExchange.Application.Queries.CurrencyRates.GetLatest;
+using CurrencyExchange.Tests.TestData;
 using FluentAssertions;
 
 namespace CurrencyExchange.Tests.UnitTests
 {
     [TestFixture]
-    public class FluentValidatorsTests
+    public class FluentValidatorsTests : FluentValidationsTestData
     {
-        public static readonly object[] ConvertCurrencyTestCases =
-        {
-            new object[] { "", "ZAR", 100M, new List<string> { "Base is required!" } },
-            new object[] { "USD", "", 100M, new List<string> { "Target is required!" } },
-            new object[] { "USD", "ZAR", 0M, new List<string> { "Amount is required!"} },
-            new object[] { "", "", 0M, new List<string> { "Base is required!", "Target is required!", "Amount is required!" } },
-        };
-        
-        public static readonly object[] AddCurrencyRatesTestCases =
-        {
-            new object[] { "", "rate results", new List<string> { "Base is required!" } },
-            new object[] { "USD", "", new List<string> { "Results is required!" } },
-            new object[] { "", "", new List<string> { "Base is required!", "Results is required!" } },
-        };
-
-        public static readonly object[] AddCurrencyConversionTestCases =
-        {
-            new object[] { "", "currency result", new List<string> { "Base is required!" } },
-            new object[] { "USD", "", new List<string> { "Result is required!" } },
-            new object[] { "", "", new List<string> { "Base is required!", "Result is required!" } },
-        };
-
         [Test]
         public void Validate_Given_Empty_Base_Of_GetLatestCurrencyQuery_Should_Fail_ValidationAnd_Return_Errors()
         {
